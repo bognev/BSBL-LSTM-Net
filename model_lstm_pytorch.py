9483879adf063549469fba044c4e96a7cc8fb434
bognev <bognev@mail.ru>
2019-03-05 23:00:35 +0300

    SBL_LSTM_STACK written in PYTORCH

--- model_lstm_pytorch.py
+++ model_lstm_pytorch.py
@@ -15,6 +15,8 @@ class SBL_LSTM_STACK(nn.Module)
         self.x_size = []
         self.prev_c = []
         self.prev_h = []
+        self.sigmoid = torch.nn.Sigmoid()
+        self.tanh = torch.nn.Tanh()
         for L in range(self.num_layers):
             if L == 1:
                 self.x.append(input)
@@ -30,12 +32,29 @@ class SBL_LSTM_STACK(nn.Module)
             self.prev_h.append(prev_hs[L])
 
 
+
     def forward(self, input, prev_hs, prev_cs):
         for L in range(self.num_layers):
-            if L == 1:
-                i2h = self.l_i2h(self.x[L])
-                h2h = self.l_h2h(self.prev_h[L])
-                all_sums = torch.add(torch.cat(i2h, h2h))
+            i2h = self.l_i2h(self.x[L])
+            h2h = self.l_h2h(self.prev_h[L])
+            all_sums = i2h + h2h
+            (n1, n2, n3, n4) = torch.split(all_sums, self.rnn_size, dim=1)  # it should return 4 tensors
+            in_gate = self.sigmoid(n1)
+            forget_gate = self.sigmoid(n3)
+            out_gate = self.sigmoid(n3)
+            in_transform = self.tanh(n4)
+            next_c1 = forget_gate*self.prev_c[L]
+            next_c2 = in_gate*in_transform
+            next_c = next_c1 + next_c2
+            next_h = out_gate * self.tanh(next_c)
+            self.next_hs[L] = next_h
+            self.next_cs[L] = next_c
+        return self.next_hs, self.next_cs
+
+
+
+
+
 
 
 
