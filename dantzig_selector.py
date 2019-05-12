15bc62f6f704e791ef2426589502b7a851715e6f
bognev <bognev@mail.ru>
2019-05-12 21:44:15 +0300

    Group Dantzig Selector + group lasso

--- dantzig_selector.py
+++ dantzig_selector.py
@@ -25,7 +25,7 @@ max_grad_norm = 5.0
 clip_gradient = 4.0
 batch_data = torch.zeros(batch_size, input_size)
 batch_label = torch.zeros(batch_size, num_nonz) # for MultiClassNLLCriterion LOSS
-lmbd = 100
+lmbd = 200
 # torch.manual_seed(10)
 def gen_groups(F, num_groups, output_size, input_size, num_nonz):
     mat_A = np.random.randn(num_groups,output_size,input_size)
@@ -35,7 +35,7 @@ def gen_groups(F, num_groups, output_size, input_size, num_nonz):
     for i in range(1,num_groups):
         label = np.concatenate((label, perm[range(num_nonz)]+i*input_size))
     x=np.zeros((num_groups*input_size))
-    x[label]=3*np.random.randn(num_groups*num_nonz)
+    x[label]=3*np.ones(num_groups*num_nonz)#*np.random.randn(num_groups*num_nonz)
     for i in range(num_groups):
         A[output_size*i:output_size*(i+1),i*input_size:(i+1)*input_size] = mat_A[i]
     n = np.random.randn(int(num_groups * output_size))
@@ -46,6 +46,7 @@ def gen_groups(F, num_groups, output_size, input_size, num_nonz):
 F = np.random.randn(int(output_size),output_size*num_groups)
 y, A, x, y_f = gen_groups(F, num_groups, output_size, input_size, num_nonz)
 
+#meazurement group lasso
 lambdas = cp.Parameter(nonneg=True)
 lambdas.value = lmbd
 # Define problem
@@ -53,18 +54,14 @@ x_v = cp.Variable(input_size*num_groups)
 p = cp.Variable(1)
 q = cp.Variable(1)
 objective = 0.5*p**2+lambdas*q
-
 a = []
 for ii in range(input_size):
     a.append(cp.norm(x_v[ii:input_size*num_groups:input_size],2))
-
 constr = [cp.norm(y_f-F@A@x_v,2) <= p, sum(a) <= q]
 prob = cp.Problem(cp.Minimize(objective), constr)
 prob.solve()
-# a_v = np.zeros((input_size))
-# for i in range(input_size):
-#     a_v[i] = a[i].value
 
+#y group lasso
 lambdas = cp.Parameter(nonneg=True)
 lambdas.value = lmbd
 # Define problem
@@ -81,17 +78,16 @@ constr = [cp.norm(y-A@x_y,2) <= p, sum(a) <= q]
 prob = cp.Problem(cp.Minimize(objective), constr)
 prob.solve()
 
+#meazurement group dantzig selector as lasso
 lambdas_f = cp.Parameter(nonneg=True)
 lambdas_f.value = lmbd
 # Define problem
 x_v_f = cp.Variable(input_size*num_groups)
 p_f = cp.Variable(1)
 q_f = cp.Variable(1)
-
 G = np.zeros((input_size,num_groups,output_size))
 a_f = []
 d_f = []
-
 for ii in range(input_size):
     G[ii,:,:] = (F@A[:,ii:input_size*num_groups:input_size]).transpose()
 for ii in range(input_size):
@@ -102,22 +98,71 @@ d_f_max = cp.norm(cp.abs(d_f[0]),2)
 for ii in range(1,input_size):
     d_f_max = cp.maximum(cp.norm(cp.abs(d_f[ii]),2), d_f_max)
 objective_f = 0.5*p_f**2+lambdas_f*q_f
-constr_f = [d_f_max <= p_f, cp.sum(d_f) <= q_f]
+constr_f = [d_f_max <= p_f, cp.sum(a_f) <= q_f]
+prob_f = cp.Problem(cp.Minimize(objective_f), constr_f)
+prob_f.solve()
+
+#meazurement group dantzig selector (4) from article
+lambdas_f = cp.Parameter(nonneg=True)
+lambdas_f.value = lmbd
+# Define problem
+x_v_e = cp.Variable(input_size*num_groups)
+G = np.zeros((input_size,num_groups,output_size))
+a_f = []
+d_f = []
+
+for ii in range(input_size):
+    G[ii,:,:] = (F@A[:,ii:input_size*num_groups:input_size]).transpose()
+for ii in range(input_size):
+    a_f.append(cp.norm(x_v_e[ii:input_size*num_groups:input_size],2))
+for ii in range(input_size):
+    d_f.append(G[ii]@(y_f-F@A@x_v_e))
+d_f_max = cp.norm(cp.abs(d_f[0]),2)
+for ii in range(1,input_size):
+    d_f_max = cp.maximum(cp.norm(cp.abs(d_f[ii]),2), d_f_max)
+objective_f = cp.sum(a_f)
+constr_f = [d_f_max <= lambdas_f]
+prob_f = cp.Problem(cp.Minimize(objective_f), constr_f)
+prob_f.solve()
+
+#y group dantzig selector (4) from article
+lambdas_f = cp.Parameter(nonneg=True)
+lambdas_f.value = lmbd
+# Define problem
+x_v_y = cp.Variable(input_size*num_groups)
+G = np.zeros((input_size,num_groups,output_size*num_groups))
+a_f = []
+d_f = []
+for ii in range(input_size):
+    G[ii,:,:] = (A[:,ii:input_size*num_groups:input_size]).transpose()
+    a_f.append(cp.norm(x_v_y[ii:input_size*num_groups:input_size],2))
+    d_f.append(G[ii]@(y-A@x_v_y))
+d_f_max = cp.norm(cp.abs(d_f[0]),2)
+for ii in range(1,input_size):
+    d_f_max = cp.maximum(cp.norm(cp.abs(d_f[ii]),2), d_f_max)
+objective_f = cp.sum(a_f)
+constr_f = [d_f_max <= lambdas_f]
 prob_f = cp.Problem(cp.Minimize(objective_f), constr_f)
 prob_f.solve()
 
 plt.figure(1)
-plt.subplot(411)
+plt.subplot(611)
 plt.plot(x, lw=2)
 plt.grid(True)
-plt.subplot(412)
+plt.subplot(612)
 plt.plot(x_v.value, lw=2)
 plt.grid(True)
-plt.subplot(413)
+plt.subplot(613)
+plt.plot(x_y.value, lw=2)
+plt.grid(True)
+plt.subplot(614)
 plt.plot(x_v_f.value, lw=2)
 plt.grid(True)
-plt.subplot(414)
-plt.plot(x_y.value, lw=2)
+plt.subplot(615)
+plt.plot(x_v_e.value, lw=2)
+plt.grid(True)
+plt.subplot(616)
+plt.plot(x_v_y.value, lw=2)
 plt.grid(True)
 plt.tight_layout()
 #
