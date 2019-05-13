ab771ede18fb9d7a309851429d582aa1ee1bc585
zabolotsky <bognev@mail.ru>
2019-05-13 15:53:21 +0300

    meazurement matrix do not work correctly

--- dantzig_selector.py
+++ dantzig_selector.py
@@ -17,7 +17,7 @@ plt.close('all')
 dataset = 'uniform' # type of non-zero elements: uniform ([-1,-0.1]U[0.1,1]), unit (+-1)
 num_nonz = 3 # number of non-zero elemetns to recovery: 3,4,5,6,7,8,9,10
 input_size = 20 # dimension of observation vector y
-output_size = 100 # dimension of sparse vector x
+output_size = 300 # dimension of sparse vector x
 gpu = 1 # gpu id
 batch_size = 1#250 # training batch size
 num_groups = 4
@@ -25,17 +25,21 @@ max_grad_norm = 5.0
 clip_gradient = 4.0
 batch_data = torch.zeros(batch_size, input_size)
 batch_label = torch.zeros(batch_size, num_nonz) # for MultiClassNLLCriterion LOSS
-lmbd = 200
+lmbd = 10
+F_m = int(num_groups*output_size/2)
+print(F_m)
 # torch.manual_seed(10)
 def gen_groups(F, num_groups, output_size, input_size, num_nonz):
+    # np.random.seed(100)
     mat_A = np.random.randn(num_groups,output_size,input_size)
-    A = np.zeros((num_groups*output_size, num_groups*input_size))
+    # A = np.random.randn(num_groups*output_size, num_groups*input_size)
+    A = np.zeros((num_groups * output_size, num_groups * input_size))
     perm = np.random.permutation(range(input_size))
     label = perm[range(num_nonz)]
     for i in range(1,num_groups):
         label = np.concatenate((label, perm[range(num_nonz)]+i*input_size))
     x=np.zeros((num_groups*input_size))
-    x[label]=3*np.ones(num_groups*num_nonz)#*np.random.randn(num_groups*num_nonz)
+    x[label]=1*np.ones(num_groups*num_nonz)#*np.random.randn(num_groups*num_nonz)
     for i in range(num_groups):
         A[output_size*i:output_size*(i+1),i*input_size:(i+1)*input_size] = mat_A[i]
     n = np.random.randn(int(num_groups * output_size))
@@ -43,7 +47,7 @@ def gen_groups(F, num_groups, output_size, input_size, num_nonz):
     y_f = F@y
     return y,A,x,y_f
 
-F = np.random.randn(int(output_size),output_size*num_groups)
+F = np.random.rand(int(F_m),output_size*num_groups)
 y, A, x, y_f = gen_groups(F, num_groups, output_size, input_size, num_nonz)
 
 #meazurement group lasso
@@ -85,14 +89,12 @@ lambdas_f.value = lmbd
 x_v_f = cp.Variable(input_size*num_groups)
 p_f = cp.Variable(1)
 q_f = cp.Variable(1)
-G = np.zeros((input_size,num_groups,output_size))
+G = np.zeros((input_size,num_groups,F_m))
 a_f = []
 d_f = []
 for ii in range(input_size):
     G[ii,:,:] = (F@A[:,ii:input_size*num_groups:input_size]).transpose()
-for ii in range(input_size):
     a_f.append(cp.norm(x_v_f[ii:input_size*num_groups:input_size],2))
-for ii in range(input_size):
     d_f.append(G[ii]@(y_f-F@A@x_v_f))
 d_f_max = cp.norm(cp.abs(d_f[0]),2)
 for ii in range(1,input_size):
@@ -107,19 +109,17 @@ lambdas_f = cp.Parameter(nonneg=True)
 lambdas_f.value = lmbd
 # Define problem
 x_v_e = cp.Variable(input_size*num_groups)
-G = np.zeros((input_size,num_groups,output_size))
+G = np.zeros((input_size,num_groups,F_m))
 a_f = []
 d_f = []
 
 for ii in range(input_size):
     G[ii,:,:] = (F@A[:,ii:input_size*num_groups:input_size]).transpose()
-for ii in range(input_size):
     a_f.append(cp.norm(x_v_e[ii:input_size*num_groups:input_size],2))
-for ii in range(input_size):
     d_f.append(G[ii]@(y_f-F@A@x_v_e))
-d_f_max = cp.norm(cp.abs(d_f[0]),2)
+d_f_max = cp.norm((d_f[0]),2)
 for ii in range(1,input_size):
-    d_f_max = cp.maximum(cp.norm(cp.abs(d_f[ii]),2), d_f_max)
+    d_f_max = cp.maximum(cp.norm(d_f[ii],1), d_f_max)
 objective_f = cp.sum(a_f)
 constr_f = [d_f_max <= lambdas_f]
 prob_f = cp.Problem(cp.Minimize(objective_f), constr_f)
